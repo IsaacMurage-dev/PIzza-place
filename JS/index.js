2cc6b90c861825aa8bf53b3047498cdf6897be3e
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-07-31 18:15:23 +0300

    enhance toggle effect on me

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#d1").click(function(){
+        $(".d2").toggle()
+
+    });
+    $("#dev1").click(function(){
+        $(".dev2").toggle()
+    });
+    $("#prd1").click(function(){
+        $(".prd2").toggle()
+    });
+    $("#ital1").click(function(){
+        $(".itali2").toggle()
+    });
+
+});
+// animation
+// $(document).click(function(){
+//         $(".s1").animate({Height:"300px"});
+// });
