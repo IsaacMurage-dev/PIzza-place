12b8913a200163d31833568657db413625962864
IsaacMurage-softwaredeveloper <isaac.moringa@student.moringaschool.com>
2021-07-31 20:10:44 +0300

    update menu sec.

--- JS/index.js
+++ JS/index.js
@@ -6,12 +6,35 @@ $(document).ready(function(){
     $("#dev1").click(function(){
         $(".dev2").toggle()
     });
+    $("#s1").click(function(){
+        $(".s2").toggle()
+    });
+    $("#s3").click(function(){
+        $(".s4").toggle()
+    });
     $("#prd1").click(function(){
         $(".prd2").toggle()
     });
     $("#ital1").click(function(){
         $(".itali2").toggle()
     });
+    $("#marg1").click(function(){
+        $(".marg2").toggle()
+    });
+    $("#pine1").click(function(){
+        $(".pine2").toggle()
+    });
+    $("#heart1").click(function(){
+        $(".heart2").toggle()
+    });
+    $("#veggie1").click(function(){
+        $(".veggie2").toggle()
+    });
+    
+    
+    
+
+
 
 });
 // animation
